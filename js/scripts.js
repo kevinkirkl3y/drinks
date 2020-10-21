ff62e46e94f6d050275f404f967c10fa5aab5f2a
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-21 16:23:04 -0700

    removed console logs
    Co-authored-by: Maxi Benbassat <info@benbassatdigital.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age > 21 < 85) {
+  if (age > 21 > 85) {
     $('#drinks').show()
   } else if (age === 21) {
     alert("Now don't go crazy!");
-    console.log("hey now");
   } else if (age >= 85) {
     $('#drinks').show()
     alert("Drinks are on the house ya dinosaur!");
   } else {
     $('#under-21').show();
-    console.log("woooo child")
   }
 });
